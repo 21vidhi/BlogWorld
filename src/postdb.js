39dd487ae89aadb692842f25b1ea4c21a34974c8
Thakur-Vidhi <vidhithakur2003@gmail.com>
2024-04-12 22:06:58 +0530

    updated files

---
+++ src/postdb.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+async function main() {
+  // mongoose.connect("mongod://localhost:27017/todolistDb", {
+  //   useNewUrlParser: true,
+  // });
+  await mongoose.connect(
+   "mongodb+srv://muskan:moveon@blogwebsite.jue2qrr.mongodb.net/blogwebsite?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  );
+  // mongoose.connect("mongodb://127.0.0.1:27017/blogWorld") //for local DB
+}
+main();
+
+const postSchema = new mongoose.Schema({
+    author: String,
+    title: String,
+    content: String,
+    thumbnail:String,
+    date:Number,
+    like:Number,
+    likedby:[String]
+    });
+    
+
+const PosT = mongoose.model("post", postSchema);
+module.exports = PosT
